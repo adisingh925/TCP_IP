8336549d02af1bdf754f7c7c1a2298a54fb76b74
Aditya <adisingh925@gmail.com>
2023-01-27 21:08:01 +0530

    NA

--- app/src/main/java/com/adreal/tcp_ip/DataClass/ConnectionData.kt
+++ app/src/main/java/com/adreal/tcp_ip/DataClass/ConnectionData.kt
@@ -2,12 +2,13 @@ package com.adreal.tcp_ip.DataClass
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity("ConnectionTable", primaryKeys = ["userId","ip"])
+@Entity("ConnectionTable", primaryKeys = ["userId"], indices = [Index(value = ["ip"], unique = true)])
 data class ConnectionData(
-    @ColumnInfo("userId") val userId : String,
-    @ColumnInfo("ip") val ip : String,
+    val userId : String,
+    val ip : String,
     val port : String,
     val token : String,
     var status : String
